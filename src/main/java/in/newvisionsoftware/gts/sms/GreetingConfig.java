51830a80a1d99f690f6792733dbc1b8a97a880da
quarkusio <no-reply@quarkus.io>
2021-09-29 16:21:53 +0000

    Initial commit

---
+++ src/main/java/in/newvisionsoftware/gts/sms/GreetingConfig.java
@@ -0,0 +1,12 @@
+package in.newvisionsoftware.gts.sms;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
